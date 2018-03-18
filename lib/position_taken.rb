37b34fedc2c093f08a62eefd6e5f854942b845ce
Chris Dundon <chris.dundon@gmail.com>
2018-03-18 01:54:34 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,10 @@
 # code your #position_taken? method here!
  
 def position_taken(board, idx)
+  if board.length = 0
+    return nil
+  end
+  
   occupant = board[idx].strip
   if occupant == ""
     return false
